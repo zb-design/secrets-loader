020455afc8efe7affb1d0f904f49f048392fd489
bfutterleib <benjamin.futterleib@gmail.com>
2024-07-25 12:29:08 +1000

    restructure into class and inject logger

--- dist/index.js
+++ dist/index.js
@@ -32,108 +32,116 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.SecretsLoader = void 0;
 require("dotenv/config");
 const identity_1 = require("@azure/identity");
 const keyvault_secrets_1 = require("@azure/keyvault-secrets");
 const fs = __importStar(require("fs"));
-// Steps
-// 1) load and env variables from .env file as normal
-// 2) load required-env file into mem
-// 3) iterate required envs, transform - into _ for purposes of matching
-// if value = FROM_SECRET load from secret store
-// 
-// working
-/**
- * Takes our required env json object and validates that each required env var has already been
- * provided through process.env as expected. Any missing required env var keys are stored in a list
- * and returned as well
- * @param requiredEnvManifest our loaded json object taken straight from the required-env.json file
- * @returns list of objects representing the env variables our application needs and a possible list
- * of missing env variable keys
- */
-function createEnvironmentVariableList(requiredEnvManifest) {
-    const appVariables = [];
-    const missingVariables = [];
-    for (const [key, value] of Object.entries(requiredEnvManifest)) {
-        // validate that each required env is already loaded into process.env first
-        if (!process.env.hasOwnProperty(key)) {
-            console.log(`[ERR] missing required env var ${key}`);
-            missingVariables.push(key);
+class SecretsLoader {
+    constructor(vaultName, moduleOptions, logger = console) {
+        this.vaultUrl = `https://${vaultName}.vault.azure.net`;
+        this.vaultClient = new keyvault_secrets_1.SecretClient(this.vaultUrl, new identity_1.DefaultAzureCredential());
+        if (!moduleOptions) {
+            this.moduleOptions = {
+                envManifest: "required-env.json",
+                secretStore: "Azure",
+                exitOnMissing: true
+            };
         }
-        const newVar = {
-            localKey: key,
-            keyVaultKey: key.replace("_", "-"), //azure key vault only allows - instead of _
-            value: process.env[key],
-            dataType: String(value)
+        else {
+            this.moduleOptions = moduleOptions;
+        }
+        this.logger = logger;
+    }
+    /**
+     * Takes our required env json object and validates that each required env var has already been
+     * provided through process.env as expected. Any missing required env var keys are stored in a list
+     * and returned as well
+     * @param requiredEnvManifest our loaded json object taken straight from the required-env.json file
+     * @returns list of objects representing the env variables our application needs and a possible list
+     * of missing env variable keys
+     */
+    createEnvironmentVariableList(requiredEnvManifest) {
+        const appVariables = [];
+        const missingVariables = [];
+        for (const [key, value] of Object.entries(requiredEnvManifest)) {
+            // validate that each required env is already loaded into process.env first
+            if (!process.env.hasOwnProperty(key)) {
+                this.logger.log(`[ERR] missing required env var ${key}`);
+                missingVariables.push(key);
+            }
+            const newVar = {
+                localKey: key,
+                keyVaultKey: key.replace("_", "-"), //azure key vault only allows - instead of _
+                value: process.env[key],
+                dataType: String(value)
+            };
+            appVariables.push(newVar);
+        }
+        return {
+            envVariableList: appVariables,
+            missingVariables: missingVariables
         };
-        appVariables.push(newVar);
     }
-    return {
-        envVariableList: appVariables,
-        missingVariables: missingVariables
-    };
-}
-function loadSecretsFromStore(client, variableList) {
-    return __awaiter(this, void 0, void 0, function* () {
-        for (let item of variableList) {
-            if (item.value === "FROM_SECRET") {
-                const secret = yield client.getSecret(item.keyVaultKey);
-                item.value = secret.value;
+    loadSecretsFromStore(client, variableList) {
+        return __awaiter(this, void 0, void 0, function* () {
+            for (let item of variableList) {
+                if (item.value === "FROM_SECRET") {
+                    const secret = yield client.getSecret(item.keyVaultKey);
+                    item.value = secret.value;
+                }
             }
-        }
-    });
-}
-function loadIntoProcessEnv(variableList) {
-    return __awaiter(this, void 0, void 0, function* () {
+        });
+    }
+    loadIntoProcessEnv(variableList) {
+        return __awaiter(this, void 0, void 0, function* () {
+            for (let item of variableList) {
+                process.env[item.localKey] = item.value;
+                this.logger.debug(`[INFO] loaded variable ${item.localKey} with value ${item.value}`);
+            }
+        });
+    }
+    typeCheckVariables(variableList) {
         for (let item of variableList) {
-            process.env[item.localKey] = item.value;
-            console.log(`[INFO] loaded variable ${item.localKey} with value ${item.value}`);
-        }
-    });
-}
-function typeCheckVariables(variableList) {
-    for (let item of variableList) {
-        const requiredDataType = item.dataType;
-        if (typeof requiredDataType !== requiredDataType) {
-            console.log(`[ERR] '${item.localKey}' variable required datatype '${requiredDataType}' does not match '${typeof requiredDataType}'`);
+            const requiredDataType = item.dataType;
+            if (typeof requiredDataType !== requiredDataType) {
+                this.logger.log(`[ERR] '${item.localKey}' variable required datatype '${requiredDataType}' does not match '${typeof requiredDataType}'`);
+            }
         }
     }
+    configure() {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const requiredEnvManifest = JSON.parse(fs.readFileSync(this.moduleOptions.envManifest, 'utf8'));
+                const result = this.createEnvironmentVariableList(requiredEnvManifest);
+                if (result.missingVariables.length) {
+                    this.logger.log("[ERR] missing the following required environment variables");
+                    this.logger.log(JSON.stringify(result.missingVariables));
+                    if (this.moduleOptions.exitOnMissing)
+                        throw new Error("Missing reqired env vars");
+                }
+                this.typeCheckVariables(result.envVariableList);
+                yield this.loadSecretsFromStore(this.vaultClient, result.envVariableList);
+                this.loadIntoProcessEnv(result.envVariableList);
+            }
+            catch (error) {
+                if (error.code === "ENOENT" && error.path === this.moduleOptions.envManifest) {
+                    this.logger.log(`Error: secrets loader unable to find required env manifest file '${this.moduleOptions.envManifest}'`);
+                }
+                else if (error.name === "AggregateAuthenticationError") {
+                    this.logger.log("Error: secrets loader authentication to Azure secret store failed");
+                }
+                process.exit(1);
+            }
+        });
+    }
 }
-function main(vaultName_1) {
-    return __awaiter(this, arguments, void 0, function* (vaultName, moduleOptions = {
-        envManifest: "required-env.json",
-        secretStore: "Azure",
-        exitOnMissing: true
-    }) {
-        // Build the URL to reach your key vault
-        const vaultClient = new keyvault_secrets_1.SecretClient(`https://${vaultName}.vault.azure.net`, new identity_1.DefaultAzureCredential());
-        try {
-            const requiredEnvManifest = JSON.parse(fs.readFileSync(moduleOptions.envManifest, 'utf8'));
-            const result = createEnvironmentVariableList(requiredEnvManifest);
-            if (result.missingVariables.length) {
-                console.log("[ERR] missing the following required environment variables");
-                console.log(JSON.stringify(result.missingVariables));
-                if (moduleOptions.exitOnMissing)
-                    throw new Error("Missing reqired env vars");
-            }
-            typeCheckVariables(result.envVariableList);
-            yield loadSecretsFromStore(vaultClient, result.envVariableList);
-            loadIntoProcessEnv(result.envVariableList);
-        }
-        catch (error) {
-            if (error.code === "ENOENT" && error.path === moduleOptions.envManifest) {
-                console.log(`Error: secrets loader unable to find required env manifest file '${moduleOptions.envManifest}'`);
-            }
-            else if (error.name === "AggregateAuthenticationError") {
-                console.log("Error: secrets loader authentication to Azure secret store failed");
-            }
-            process.exit(1);
-        }
+exports.SecretsLoader = SecretsLoader;
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const secretLoader = new SecretsLoader(process.env.AZURE_KEY_VAULT);
+        yield secretLoader.configure();
     });
 }
-main("kv-shared-dev-southeast", {
-    envManifest: "required-env.json",
-    secretStore: "Azure",
-    exitOnMissing: false,
-});
+main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -24,95 +24,119 @@ type applicationVariable = {
 // 
 // working
 
-/**
- * Takes our required env json object and validates that each required env var has already been 
- * provided through process.env as expected. Any missing required env var keys are stored in a list
- * and returned as well
- * @param requiredEnvManifest our loaded json object taken straight from the required-env.json file
- * @returns list of objects representing the env variables our application needs and a possible list
- * of missing env variable keys
- */
-function createEnvironmentVariableList(requiredEnvManifest: any) {
-  const appVariables: applicationVariable[] = [];
-  const missingVariables: string[] = [];
-  for (const [key, value] of Object.entries(requiredEnvManifest)) {
-    // validate that each required env is already loaded into process.env first
-    if (!process.env.hasOwnProperty(key)) {
-      console.log(`[ERR] missing required env var ${key}`)
-      missingVariables.push(key);
+interface Logger {
+  log(message: string): void;
+  warn(message: string): void;
+  error(message: string): void;
+  debug(message: string): void;
+}
+
+export class SecretsLoader {
+  private vaultUrl: string;
+  private vaultClient: SecretClient;
+  private moduleOptions: moduleOptions;
+  private logger: Logger;
+
+  constructor(vaultName: string, moduleOptions?: moduleOptions, logger: Logger = console) {
+    this.vaultUrl = `https://${vaultName}.vault.azure.net`;
+    this.vaultClient = new SecretClient(this.vaultUrl, new DefaultAzureCredential());
+    if (!moduleOptions) {
+      this.moduleOptions = {
+        envManifest: "required-env.json",
+        secretStore: "Azure",
+        exitOnMissing: true
+      }
+    } else {
+      this.moduleOptions = moduleOptions;
     }
-    const newVar: applicationVariable = {
-      localKey: key,
-      keyVaultKey: key.replace("_", "-"), //azure key vault only allows - instead of _
-      value: process.env[key],
-      dataType: String(value)
-    }
-    appVariables.push(newVar);
+    this.logger = logger;
   }
-  return {
-    envVariableList: appVariables,
-    missingVariables: missingVariables
+
+
+  /**
+   * Takes our required env json object and validates that each required env var has already been 
+   * provided through process.env as expected. Any missing required env var keys are stored in a list
+   * and returned as well
+   * @param requiredEnvManifest our loaded json object taken straight from the required-env.json file
+   * @returns list of objects representing the env variables our application needs and a possible list
+   * of missing env variable keys
+   */
+  private createEnvironmentVariableList(requiredEnvManifest: any) {
+    const appVariables: applicationVariable[] = [];
+    const missingVariables: string[] = [];
+    for (const [key, value] of Object.entries(requiredEnvManifest)) {
+      // validate that each required env is already loaded into process.env first
+      if (!process.env.hasOwnProperty(key)) {
+        this.logger.log(`[ERR] missing required env var ${key}`)
+        missingVariables.push(key);
+      }
+      const newVar: applicationVariable = {
+        localKey: key,
+        keyVaultKey: key.replace("_", "-"), //azure key vault only allows - instead of _
+        value: process.env[key],
+        dataType: String(value)
+      }
+      appVariables.push(newVar);
+    }
+    return {
+      envVariableList: appVariables,
+      missingVariables: missingVariables
+    }
+  }
+  
+  private async loadSecretsFromStore(client: SecretClient, variableList: applicationVariable[]): Promise<void> {
+    for (let item of variableList) {
+      if (item.value === "FROM_SECRET") {
+        const secret = await client.getSecret(item.keyVaultKey);
+        item.value = secret.value;
+      }
+    }
+  }
+  
+  private async loadIntoProcessEnv(variableList: applicationVariable[]) {
+    for (let item of variableList) {
+      process.env[item.localKey] = item.value;
+      this.logger.debug(`[INFO] loaded variable ${item.localKey} with value ${item.value}`)
+    }
+  }
+  
+  private typeCheckVariables(variableList: applicationVariable[]): void {
+    for (let item of variableList) {
+      const requiredDataType = item.dataType;
+  
+      if (typeof requiredDataType !== requiredDataType) {
+        this.logger.log(`[ERR] '${item.localKey}' variable required datatype '${requiredDataType}' does not match '${typeof requiredDataType}'`)
+      }
+    }
+  }
+
+  public async configure() {
+    try {
+      const requiredEnvManifest = JSON.parse(fs.readFileSync(this.moduleOptions.envManifest, 'utf8'));
+      const result = this.createEnvironmentVariableList(requiredEnvManifest);
+  
+      if (result.missingVariables.length) {
+        this.logger.log("[ERR] missing the following required environment variables");
+        this.logger.log(JSON.stringify(result.missingVariables));
+        if (this.moduleOptions.exitOnMissing) throw new Error("Missing reqired env vars");
+      }
+      this.typeCheckVariables(result.envVariableList);
+      await this.loadSecretsFromStore(this.vaultClient, result.envVariableList);
+      this.loadIntoProcessEnv(result.envVariableList);
+    } catch (error) {
+      if (error.code === "ENOENT" && error.path === this.moduleOptions.envManifest) {
+        this.logger.log(`Error: secrets loader unable to find required env manifest file '${this.moduleOptions.envManifest}'`);
+      } else if (error.name === "AggregateAuthenticationError") {
+        this.logger.log("Error: secrets loader authentication to Azure secret store failed");
+      }
+      process.exit(1);
+    }
   }
 }
 
-async function loadSecretsFromStore(client: SecretClient, variableList: applicationVariable[]): Promise<void> {
-  for (let item of variableList) {
-    if (item.value === "FROM_SECRET") {
-      const secret = await client.getSecret(item.keyVaultKey);
-      item.value = secret.value;
-    }
-  }
+async function main() {
+  const secretLoader = new SecretsLoader(process.env.AZURE_KEY_VAULT);
+  await secretLoader.configure();
 }
 
-async function loadIntoProcessEnv(variableList: applicationVariable[]) {
-  for (let item of variableList) {
-    process.env[item.localKey] = item.value;
-    console.log(`[INFO] loaded variable ${item.localKey} with value ${item.value}`)
-  }
-}
-
-function typeCheckVariables(variableList: applicationVariable[]): void {
-  for (let item of variableList) {
-    const requiredDataType = item.dataType;
-
-    if (typeof requiredDataType !== requiredDataType) {
-      console.log(`[ERR] '${item.localKey}' variable required datatype '${requiredDataType}' does not match '${typeof requiredDataType}'`)
-    }
-  }
-} 
-
-async function main(vaultName: string, moduleOptions: moduleOptions = {
-  envManifest: "required-env.json",
-  secretStore: "Azure",
-  exitOnMissing: true
-}) {
-  // Build the URL to reach your key vault
-  const vaultClient = new SecretClient(`https://${vaultName}.vault.azure.net`, new DefaultAzureCredential());
-
-  try {
-    const requiredEnvManifest = JSON.parse(fs.readFileSync(moduleOptions.envManifest, 'utf8'));
-    const result = createEnvironmentVariableList(requiredEnvManifest);
-
-    if (result.missingVariables.length) {
-      console.log("[ERR] missing the following required environment variables");
-      console.log(JSON.stringify(result.missingVariables));
-      if (moduleOptions.exitOnMissing) throw new Error("Missing reqired env vars");
-    }
-    typeCheckVariables(result.envVariableList);
-    await loadSecretsFromStore(vaultClient, result.envVariableList);
-    loadIntoProcessEnv(result.envVariableList);
-  } catch (error) {
-    if (error.code === "ENOENT" && error.path === moduleOptions.envManifest) {
-      console.log(`Error: secrets loader unable to find required env manifest file '${moduleOptions.envManifest}'`);
-    } else if (error.name === "AggregateAuthenticationError") {
-      console.log("Error: secrets loader authentication to Azure secret store failed");
-    }
-    process.exit(1);
-  }
-}
-
-main("kv-shared-dev-southeast", {
-  envManifest: "required-env.json",
-  secretStore: "Azure",
-  exitOnMissing: false,
-});
+main();
